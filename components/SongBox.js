228f0784c462560b93f17d87793890ec8333bada
Hargoze <francois.lecarpentier99@gmail.com>
2021-02-03 14:18:53 +0100

    improve layout

--- components/SongBox.js
+++ components/SongBox.js
@@ -1,5 +1,6 @@
-import { Flex, Stack, Heading, Box, Text, Tag, TagLabel, Link, useColorModeValue} from '@chakra-ui/react'
+import { Flex, Stack, Text, Link, useColorModeValue} from '@chakra-ui/react'
 import { Thumbnail } from "./Thumbnail"
+import { Avatar } from "./Avatar"
 
 export const SongBox = ({songs}) => {
     const bg = useColorModeValue("gray.100", "gray.700")
@@ -8,14 +9,19 @@ export const SongBox = ({songs}) => {
     return (
       <Stack my="24" bg={bg} width="80%" color={textColor}>
         {songs.map((current, i) => (
+          <Link _hover={{textDecoration:"none"}} key={i} >
             <Flex height="180px" shadow="0 7px 29px -12px rgba(0,0,0,34)" bg={bg}
-            _hover={{textDecoration:"none", transform: "scale(1.02)"}} key={i}>
+            _hover={{textDecoration:"none", transform: "scale(1.02)"}}>
                 <Thumbnail url={current.thumbnail.url}/>
                 <Stack w="100%" alignItems="start" px="6" justify="space-around">
                    <Text fontSize="24px" >{current.title}</Text>
-                   <Text>{current.author.name}</Text> 
+                   <Flex justify="center" align="center">
+                     <Avatar url={current.author.picture.url}/>
+                     <Text fontSize="18px" pl="4">{current.author.name}</Text>
+                    </Flex>
                 </Stack>
             </Flex>
+          </Link>
         ))}
       </Stack>
     )
