c0a3dab292045b630f2e9c994d2009523489ff29
Hargoze <francois.lecarpentier99@gmail.com>
2021-04-25 17:25:03 +0200

    improved responsive for song box

--- components/SongBox.js
+++ components/SongBox.js
@@ -7,14 +7,14 @@ export const SongBox = ({songs}) => {
     const textColor = useColorModeValue("black", "white")
 
     return (
-      <Stack my="24" width={{base:"80%", lg:"40%"}} color={textColor}>
+      <Stack my="24" width={{base:"80%", md:"60%", lg:"40%"}} color={textColor} align="center" >
         {songs.map((current, i) => (
-          <Link _hover={{textDecoration:"none"}} key={i} href={`/songs/${current.id}`} my="4">
-            <Flex direction={{base: "column", lg:"row"}} shadow="0 7px 29px -12px rgba(0,0,0,34)" bg={bg}
-            _hover={{textDecoration:"none", transform: "scale(1.02)"}}>
+          <Link _hover={{textDecoration:"none"}} key={i} href={`/songs/${current.id}`} my="4" >
+            <Flex direction={{base: "column", md:"row"}} shadow="0 7px 29px -12px rgba(0,0,0,34)" bg={bg}
+            _hover={{textDecoration:"none", transform: "scale(1.02)"}} w={{base:"315px", md:"630px"}}>
                 <Thumbnail url={current.thumbnail.url} borderRadius="full"/>
                 <Stack w="100%" alignItems="start" px="6" justify="space-around" py={{base:"6",lg:"0"}}>
-                   <Text fontSize="24px" textOverflow="ellipsis" noOfLines={{base:1, lg:2}}>{current.title}</Text>
+                   <Text fontSize="24px" textOverflow="ellipsis" noOfLines={{base:1, md:2}}>{current.title}</Text>
                    <Flex justify="center" align="center">
                      <Avatar url={current.author.picture.url}/>
                      <Text fontSize="18px" pl="4">{current.author.name}</Text>
