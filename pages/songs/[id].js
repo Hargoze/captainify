94b92ec920fe7b131fc5cf3f588765b4c11b4fc4
Hargoze <francois.lecarpentier99@gmail.com>
2021-05-11 16:38:34 +0200

    own pause/play button

--- pages/songs/[id].js
+++ pages/songs/[id].js
@@ -1,19 +1,62 @@
-import ReactAudioPlayer from 'react-audio-player'
 import { Box, Stack, Flex, AspectRatio, Text, Image, Button, Checkbox} from '@chakra-ui/react'
 import { getAllSongsId, getSongById } from '../../lib/api'
 import { Header } from '../../components/Header'
 import { Container } from '../../components/Container'
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+
+const AudioControls = ({ isPlaying, onPlayPauseClick }) => (
+    <div className="audio-controls">
+      {isPlaying ? (
+        <Button
+          onClick={() => onPlayPauseClick(false)}
+          aria-label="Pause"
+        >
+          Pause
+        </Button>
+      ) : (
+        <Button
+          onClick={() => onPlayPauseClick(true)}
+          aria-label="Play"
+        >
+          Play
+        </Button>
+      )}
+    </div>
+);
 
 export default function Songs({song}) {
+    console.log(song.link)
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef(new Audio(`${song.file.url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${song.file.url}`));
+    const isReady = useRef(false);
+    useEffect(() => {
+        if (isPlaying) {
+          audioRef.current.play();
+        } else {
+          audioRef.current.pause();
+        }
+      }, [isPlaying]);
+    
+      // Handles cleanup and setup when changing tracks
+      useEffect(() => {
+        audioRef.current.pause();
+    
+        audioRef.current = new Audio(`${song.file.url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${song.file.url}`);
+    
+        if (isReady.current) {
+          audioRef.current.play();
+          setIsPlaying(true);
+        } else {
+          // Set the isReady ref as true for the next pass
+          isReady.current = true;
+        }
+      }, []);
     return (
         <Container>
             <Header />
             <Image src={`${song.thumbnail.url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${song.thumbnail.url}`}/>
-            <ReactAudioPlayer
-                src={`${song.file.url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${song.file.url}`}
-                controls
-            />
+            
+            <AudioControls isPlaying={isPlaying} onPlayPauseClick={setIsPlaying} />
         </Container>
     )
 }
