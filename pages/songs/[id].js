05efd7af090d5bbce5a454cfeb2e54ce9a6177b3
Hargoze <francois.lecarpentier99@gmail.com>
2021-05-13 09:44:55 +0200

    slider bar v1

--- pages/songs/[id].js
+++ pages/songs/[id].js
@@ -1,4 +1,5 @@
-import { Box, Stack, Flex, AspectRatio, Text, Image, Button, Checkbox} from '@chakra-ui/react'
+import { Box, Stack, Flex, AspectRatio, Text, Input, Image, Button, Checkbox} from '@chakra-ui/react'
+import { Slider, SliderTrack, SliderFilledTrack, SliderThumb} from "@chakra-ui/react"
 import { getAllSongsId, getSongById } from '../../lib/api'
 import { Header } from '../../components/Header'
 import { Container } from '../../components/Container'
@@ -25,13 +26,49 @@ const AudioControls = ({ isPlaying, onPlayPauseClick }) => (
 );
 
 export default function Songs({song}) {
-    console.log(song.link)
+    const [trackProgress, setTrackProgress] = useState(0);
     const [isPlaying, setIsPlaying] = useState(true);
+    
     const audioRef = useRef(new Audio(`${song.file.url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${song.file.url}`));
+    const intervalRef = useRef();
     const isReady = useRef(true);
+
+
+    const { duration } = audioRef.current;
+
+    const currentPercentage = duration ? `${(trackProgress / duration) * 100}%` : "0%";
+    const trackStyling = `-webkit-gradient(linear, 0% 0%, 100% 0%, color-stop(${currentPercentage}, #fff), color-stop(${currentPercentage}, #777))`;
+    const startTimer = () => {
+      // Clear any timers already running
+      clearInterval(intervalRef.current);
+
+      intervalRef.current = setInterval(() => {
+        if (audioRef.current.ended) {
+          console.log("ended")
+        } else {
+          setTrackProgress(audioRef.current.currentTime);
+        }
+      }, [1000]);
+    };
+
+    const onScrub = (value) => {
+      // Clear any timers already running
+      clearInterval(intervalRef.current);
+      audioRef.current.currentTime = value;
+      setTrackProgress(audioRef.current.currentTime);
+    };
+
+    const onScrubEnd = () => {
+      // If not already playing, start
+      if (!isPlaying) {
+        setIsPlaying(true);
+      }
+      startTimer();
+    };
     useEffect(() => {
         if (isPlaying) {
           audioRef.current.play();
+          startTimer();
         } else {
           audioRef.current.pause();
         }
@@ -57,6 +94,33 @@ export default function Songs({song}) {
             <Image src={`${song.thumbnail.url.startsWith('/') ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${song.thumbnail.url}`}/>
             
             <AudioControls isPlaying={isPlaying} onPlayPauseClick={setIsPlaying} />
+            <input
+              type="range"
+              value={trackProgress}
+              step="1"
+              min="0"
+              max={duration ? duration : `${duration}`}
+              className="progress"
+              onChange={(e) => onScrub(e.target.value)}
+              onMouseUp={onScrubEnd}
+              onKeyUp={onScrubEnd}
+              style={{ background: trackStyling }}
+          />
+            <Slider 
+              w="40%"
+              step={10}
+              value={currentPercentage}
+              min="0"
+              max="100"
+              display="none"
+              onChange={(val) => onScrub(val)}
+              onMouseUp={onScrubEnd}
+              onKeyUp={onScrubEnd}>
+              <SliderTrack>
+                <SliderFilledTrack />
+              </SliderTrack>
+              <SliderThumb />
+            </Slider>
         </Container>
     )
 }
