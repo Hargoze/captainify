286304a46b243e0ff87c00d04674c138860b8c31
Hargoze <francois.lecarpentier99@gmail.com>
2021-05-23 14:20:56 +0200

    add new graphql query

--- pages/author/[id].js
+++ pages/author/[id].js
@@ -1,13 +1,22 @@
 import ReactAudioPlayer from 'react-audio-player'
 import { Box, Stack, Flex, AspectRatio, Text, Image, Button, Checkbox} from '@chakra-ui/react'
-import { getAllSongsId, getSongById } from '../../lib/api'
+import { getAllSongsId, getAllSongsFromAuthor } from '../../lib/api'
 import { Header } from '../../components/Header'
+import { SongBox} from "../../components/SongBox"
 import { Container } from '../../components/Container'
 import React, { useEffect, useState } from 'react';
 
 //            <Image src={`${author.picture.url ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${author.picture.url}`}/>
 
+//<Text fontSize={{ base: "100px", md: "60px", lg: "76px" }}>{author.name}</Text>
+//<Image width={{base:"50%", lg:"25%"}} borderRadius="full" alt="Bioutifoule Duck" boxSize={{base:"50%", lg:"25%"}} src={`${author.picture.url ? process.env.NEXT_PUBLIC_STRAPI_API_URL : ''}${author.picture.url}`}/>
+
 export default function Author ({author}) {
+    if (!author) {
+        return (
+            <Text>Loading</Text>
+        )
+    }
     return (
         <Container>
             <Header />
@@ -18,10 +27,9 @@ export default function Author ({author}) {
 }
 
 export async function getStaticProps({ params}) {
-    const data = await getSongById(params.id)
-    const song = data.songs[0]
-    const author = song.author
-    console.log(author)
+    const data = await getAllSongsFromAuthor(params.id)
+    const author = data.authors[0]
+    //const songs = data.songs
     return {
         props: { author },
     }
